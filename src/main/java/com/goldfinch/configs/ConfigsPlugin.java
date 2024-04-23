950888772fe8837a4941b60ca5bd590e859112dd
goldfinch <dyboshit@gmail.com>
2024-04-23 10:57:56 +0200

    refactor: added plugin structure

---
+++ src/main/java/com/goldfinch/configs/ConfigsPlugin.java
@@ -0,0 +1,14 @@
+package com.goldfinch.configs;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Configs has been enabled!");
+
+    }
+
+}
